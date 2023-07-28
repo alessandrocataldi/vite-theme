dd498a5f4c439dc05812838c99acc4e7b8ba67c3
Alessandro Cataldi <alessandro.cataldi@outlook.com>
2023-07-28 15:44:33 +0200

    edit: delete file

--- src/darkMode.tsx
+++
@@ -1,9 +0,0 @@
-export const darkTheme = {
-  white: "white",
-  body: "#121620",
-  surface: "#252b3a",
-  border: "#333",
-  text: "#ffffff",
-  textEmphasis: "#ffffff",
-  accents: "#167D8E",
-};
