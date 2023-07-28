cbad6efa4306b8f6e896d6e8175036c5b7b25dfd
Alessandro Cataldi <alessandro.cataldi@outlook.com>
2023-07-28 15:45:02 +0200

    edit: add dark and light mode

---
+++ src/Theme/DarkMode.tsx
@@ -0,0 +1,10 @@
+export const darkTheme = {
+  white: "white",
+  body: "#121620",
+  surface: "#252b3a",
+  border: "#333",
+  text: "#ffffff",
+  textButton: "#fff",
+  textEmphasis: "#ffffff",
+  accent: "#167D8E",
+};
---
+++ src/Theme/LightMode.tsx
@@ -0,0 +1,10 @@
+export const LightTheme = {
+  white: "white",
+  body: "#f1f1f1",
+  surface: "#ffffff",
+  border: "#ebebeb",
+  text: "#121620",
+  textButton: "#fff",
+  textEmphasis: "#111",
+  accent: "#167D8E",
+};
